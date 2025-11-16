6baa0b44ab6dbd238846d084caca807c7f4abcc1
Jvctori <137020639+Jvctori@users.noreply.github.com>
2025-11-16 19:39:41 -0300

    balance commit

--- hello.go
+++ hello.go
@@ -5,9 +5,12 @@ import "fmt"
 func main() {
 	var idade int
 	var nome string
+	var dinheiro float64
 	fmt.Println("Digite sua idade: ")
 	fmt.Scan(&idade)
 	fmt.Println("Digite seu nome: ")
 	fmt.Scan(&nome)
-	fmt.Printf("Hello!, %s, você tem %d anos!\n", nome, idade)
+	fmt.Println("Digite seu saldo bancário: ")
+	fmt.Scan(&dinheiro)
+	fmt.Printf("Hello!, %s, você tem %d anos!\nPossui %.2f na conta\n", nome, idade, dinheiro)
 }
